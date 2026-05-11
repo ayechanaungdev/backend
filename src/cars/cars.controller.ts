c56b651a4213bc1cf3645cdc3c9e95e5d3eabd6c
ayechanaungdev <ayechanaungdev@gmail.com>
2026-05-11 13:10:24 +0630

    DAY-04 (Completed): move CarDto files into /dto folder

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CarsService } from './cars.service';
-import { CreateCarDto } from './create-car.dto';
-import { UpdateCarDto } from './update-car.dto';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateCarDto } from './create-car.dto';
-import { UpdateCarDto } from './update-car.dto';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Injectable()
 export class CarsService {
