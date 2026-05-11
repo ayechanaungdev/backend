8311d1b4775cc3acd5031c0ce1bf918c0a51ccf5
ayechanaungdev <ayechanaungdev@gmail.com>
2026-05-11 11:33:10 +0630

    DAY-04: implement CreateUserDto for data validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CarsModule } from './cars/cars.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [PrismaModule, CarsModule], // We will add other modules here later
+    imports: [PrismaModule, CarsModule, UsersModule], // We will add other modules here later
     controllers: [AppController], // Routes go here
     providers: [AppService], // Business logic goes here
 })
